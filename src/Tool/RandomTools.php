9fd55e47e137974c04338974ac1547ada449c3b3
Korbinian Ober <korbinian@dc-o.de>
2024-09-29 14:34:18 +0200

    Another Random String generator

--- src/Tool/RandomTools.php
+++ src/Tool/RandomTools.php
@@ -11,4 +11,22 @@ class RandomTools {
     public static function getRandomString($n) {
         return bin2hex(random_bytes($n / 2));
     }
+
+    /**
+     * Get a random string with $n characters from $characterList
+     * @param $n
+     * @param $characterList
+     * @return string
+     * @throws \Random\RandomException
+     */
+    public static function getRandomStringFromCharacters($n, $characterList) {
+        $charactersLength = strlen($characterList);
+        $randomString = '';
+
+        for ($i = 0; $i < $n; $i++) {
+            $randomString .= $characterList[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
 }
